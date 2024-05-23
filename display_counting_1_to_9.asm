a8a2a3dedf778b2813e006454c72c35e7ae0e971
Abdul Ahad <148152489+abdulahad0110@users.noreply.github.com>
2024-05-23 22:29:28 +0500

    print counting 1 to 9

---
+++ display_counting_1_to_9.asm
@@ -0,0 +1,37 @@
+section	.text
+	global _start      
+_start: 
+    mov ecx, 9
+    mov eax, '1'
+
+    myloop:
+    mov [num],eax
+    mov eax, 4
+    mov ebx, 1
+    push ecx
+    mov ecx, num
+    mov edx, 1
+	int	0x80 
+	
+	mov eax, 4
+	mov ebx, 1
+	mov ecx , m
+	mov edx, l
+	int 0x80
+	
+	mov eax, [num]
+	sub eax, '0'
+	inc eax
+	add eax, '0'
+	pop ecx
+	
+	
+	loop myloop
+	mov	eax, 1	    
+	int	0x80        
+
+section	.data
+m db ' '
+l equ $ - m
+section .bss
+num resb 1
